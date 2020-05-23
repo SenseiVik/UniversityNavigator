38a00d1c3cd4deb5ef075086fef48cb1880b1a72
Victor <senseyvik22@gmail.com>
2020-05-24 01:20:29 +0300

    updated nugets, added sql scripts, start work with main html page

---
+++ CreateDatabase.sql
@@ -0,0 +1,76 @@
+﻿CREATE DATABASE [UniversityNavigator];
+GO
+
+USE [UniversityNavigator]
+GO
+/****** Object:  Table [dbo].[Audience]    Script Date: 05/24/2020 1:04:03 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Audience](
+	[AudienceId] [varchar](30) NOT NULL,
+	[Floor] [int] NOT NULL,
+	[Building] [varchar](1) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[AudienceId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AudienceImage]    Script Date: 05/24/2020 1:04:03 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AudienceImage](
+	[Audience] [varchar](30) NOT NULL,
+	[ImagePath] [varchar](max) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Audience] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[AudienceQuickSearch]    Script Date: 05/24/2020 1:04:03 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AudienceQuickSearch](
+	[Audience] [varchar](30) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Audience] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AudienceImage]  WITH CHECK ADD  CONSTRAINT [FK_AudienceImageRef] FOREIGN KEY([Audience])
+REFERENCES [dbo].[Audience] ([AudienceId])
+GO
+ALTER TABLE [dbo].[AudienceImage] CHECK CONSTRAINT [FK_AudienceImageRef]
+GO
+ALTER TABLE [dbo].[AudienceQuickSearch]  WITH CHECK ADD  CONSTRAINT [FK_AudienceQuickSearch] FOREIGN KEY([Audience])
+REFERENCES [dbo].[Audience] ([AudienceId])
+GO
+ALTER TABLE [dbo].[AudienceQuickSearch] CHECK CONSTRAINT [FK_AudienceQuickSearch]
+GO
+/****** Object:  Trigger [dbo].[AudienceOnInsertTrigger]    Script Date: 05/24/2020 1:04:03 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create trigger [dbo].[AudienceOnInsertTrigger]
+on [dbo].[Audience]
+after insert
+as
+begin
+	set nocount on;
+	insert into AudienceQuickSearch(Audience)
+	select i.AudienceId from inserted as i
+end
+
+GO
+ALTER TABLE [dbo].[Audience] ENABLE TRIGGER [AudienceOnInsertTrigger]
+GO
--- WEB/UniversityNavigator/wwwroot/js/site.js
+++ WEB/UniversityNavigator/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Material Select Initialization
+
+// function for data load at seach btn submit
+function submitSearch(event) {
+    let audience = $("#search-btn").val();
+
+    await $.ajax({
+        type: 'GET',
+        url: '',
+        data: { audienceId = audienceId }
+    }).done(function (img) {
+
+    })
+}
+
+$(document).ready(function () {
+    $("#search-btn").click(submitSearch);
+});
