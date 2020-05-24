f4d296f5f789cf3da77c4fc0548b9ee08796c023
Victor <senseyvik22@gmail.com>
2020-05-24 23:54:29 +0300

    add animation

--- WEB/UniversityNavigator/wwwroot/js/site.js
+++ WEB/UniversityNavigator/wwwroot/js/site.js
@@ -7,20 +7,31 @@
 // function for data load at seach btn submit
 
 function fillAudienceInfo(data) {
+    $("#audience-data").hide("400");
+
+    $("#audience-name").text(`Аудиторія: ${data[0].audienceId}`);
+    $("#audience-building").text(`Корпус: ${data[0].building}`);
+    $("#audience-floor").text(`Поверх: ${data[0].floor}`);
     $("#audience-image").attr("src", `data:image/jpeg;base64,${data[0].imageBytes}`);
+
+    $("#audience-data").show("slow");
 }
 
 
-async function submitSearch(event) {
+function submitSearch(event) {
     let _audience = $("#input_search").val();
     let _url = "https://localhost:44395/api/universitynavigator/audience/" + _audience;
 
-    await $.ajax({
+    $.ajax({
         type: 'GET',
         url: _url,
     }).done(data => fillAudienceInfo(data));
+
 }
 
 $(document).ready(function () {
+    $("#audience-data").hide();
+
     $("#search-btn").click(submitSearch);
+    })
 });
